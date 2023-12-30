3747c5d3b3770b1ea1d88708ec9f11e42ca99317
PRITAM CHAKROBORTY <chakrobortypritam1@gmail.com>
2023-12-30 19:07:19 +0530

    LogOut view done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
+import LogOutView from "./Components/LogOutView"
 
 
 function App() {
 
   return (
     <>
-      <h1 className="text-5xl uppercase text-red-300 font-bold underline">
-        Hello world!
-      </h1>
+      <LogOutView />
     </>
   )
 }
